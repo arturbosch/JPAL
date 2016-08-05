2936328b523d7eb644c668827d492c3aae81a862
ArtiSmarti <arturbosch@gmx.de>
2016-08-05 23:31:07 +0200

    Increase code coverage of qualified type to 100

--- src/test/groovy/io/gitlab/arturbosch/jpal/resolve/QualifiedTypeTest.groovy
+++ src/test/groovy/io/gitlab/arturbosch/jpal/resolve/QualifiedTypeTest.groovy
@@ -37,4 +37,43 @@ class QualifiedTypeTest extends Specification {
 		then: "returned class should be outer class"
 		outerType == outerClass
 	}
+
+	def "test additional methods on qualified type"() {
+		given: "a inner class"
+		QualifiedType cycleType = new QualifiedType("io.gitlab.arturbosch.jpal.CycleDummy.InnerCycleOne",
+				QualifiedType.TypeToken.REFERENCE)
+
+		when: "calling as outer class"
+		def outerType = cycleType.asOuterClass()
+
+		then: "it becomes an outer class type"
+		outerType.name == "io.gitlab.arturbosch.jpal.CycleDummy"
+
+		when: "retrieving path to its file"
+		def javaFile = cycleType.asStringPathToJavaFile()
+
+		then: "package structure until outer class is given as string"
+		javaFile == "io/gitlab/arturbosch/jpal/CycleDummy.java"
+
+		when: "calling short name"
+		def shortName = cycleType.shortName()
+
+		then: "only the inner class name is returned"
+		shortName == "InnerCycleOne"
+
+		when: "verifying type"
+		then: "only the reference type is true"
+		!cycleType.isFromJdk()
+		!cycleType.isPrimitive()
+		cycleType.isReference()
+	}
+
+	def "the short name of not a qualified outer type is just the class name"() {
+		expect:
+		type.shortName() == "CycleDummy"
+		!type.isInnerClass()
+		where:
+		type = new QualifiedType("CycleDummy", QualifiedType.TypeToken.REFERENCE)
+	}
+
 }
