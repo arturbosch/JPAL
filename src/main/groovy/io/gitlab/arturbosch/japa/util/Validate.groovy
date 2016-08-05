a50c4fab11509730e87393f2b86570adab0513e4
ArtiSmarti <arturbosch@gmx.de>
2016-08-05 19:52:16 +0200

    Add util classes for validation

---
+++ src/main/groovy/io/gitlab/arturbosch/japa/util/Validate.groovy
@@ -0,0 +1,30 @@
+package io.gitlab.arturbosch.japa.util;
+
+/**
+ * @author artur
+ */
+public final class Validate {
+
+	private Validate() {
+	}
+
+	public static void isTrue(final boolean expression, final String message) {
+		if (!expression) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static <T> T notNull(T object) {
+		if (object == null) {
+			throw new IllegalArgumentException("Provided argument is null!");
+		}
+		return object;
+	}
+
+	public static <T> T notNull(T object, String message) {
+		if (object == null) {
+			throw new IllegalArgumentException(message);
+		}
+		return object;
+	}
+}
