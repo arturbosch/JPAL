404662aa7f05ab4ceccfd2e8b295a0e7c5414b56
ArtiSmarti <arturbosch@gmx.de>
2016-08-07 09:57:18 +0200

    Document locale variable helper

--- src/main/groovy/io/gitlab/arturbosch/jpal/nested/NoClassesException.groovy
+++ src/main/groovy/io/gitlab/arturbosch/jpal/nested/NoClassesException.groovy
@@ -3,6 +3,9 @@ package io.gitlab.arturbosch.jpal.nested
 import groovy.transform.CompileStatic
 
 /**
+ * Thrown by inner classes handler when the given compilation unit is invalid -
+ * contains no classes.
+ *
  * @author artur
  */
 @CompileStatic
