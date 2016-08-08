3aae1c275fde6bc2757abcd70e690d1eb056607a
ArtiSmarti <arturbosch@gmx.de>
2016-08-08 11:00:08 +0200

    Write test case for inner classes handler

---
+++ src/test/groovy/io/gitlab/arturbosch/jpal/nested/InnerClassesHandlerTest.groovy
@@ -0,0 +1,21 @@
+package io.gitlab.arturbosch.jpal.nested
+
+import com.github.javaparser.ast.type.ClassOrInterfaceType
+import io.gitlab.arturbosch.jpal.Helper
+import spock.lang.Specification
+
+/**
+ * @author artur
+ */
+class InnerClassesHandlerTest extends Specification {
+
+	def "domain tests"() {
+		given: "inner classes handler"
+		def handler = new InnerClassesHandler(Helper.compile(Helper.CYCLE_DUMMY))
+		when: "asking for the composite name of outer class and inner class - InnerCycleOne"
+		def name = handler.getUnqualifiedNameForInnerClass(new ClassOrInterfaceType("InnerCycleOne"))
+		then: "the right name is returned"
+		name == "CycleDummy.InnerCycleOne"
+	}
+
+}
