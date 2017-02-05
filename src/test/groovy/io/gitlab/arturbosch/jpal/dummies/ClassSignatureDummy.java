823ce4049f1cc886c3a637598fcf02b053fe14a3
abosch <arturbosch@gmx.de>
2017-02-05 22:29:36 +0100

    Introduce enum helper which can produce signatures

--- src/test/groovy/io/gitlab/arturbosch/jpal/dummies/ClassSignatureDummy.java
+++ src/test/groovy/io/gitlab/arturbosch/jpal/dummies/ClassSignatureDummy.java
@@ -8,8 +8,17 @@ import java.util.List;
 @SuppressWarnings("ALL")
 public class ClassSignatureDummy {
 
-	class VeryComplexInnerClass<T extends String, B extends List<T>>
+	static class VeryComplexInnerClass<T extends String, B extends List<T>>
 			extends ClassSignatureDummy implements Cloneable, Runnable {
+
+		enum EnumSignatureDummy implements Runnable {
+			;
+
+			@Override
+			public void run() {
+
+			}
+		}
 
 		@Override
 		public void run() {
