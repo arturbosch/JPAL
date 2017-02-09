3e9be9cd05a7df144e159dd2539187de76ffb7c4
abosch <arturbosch@gmx.de>
2017-02-09 21:28:18 +0100

    A source path can now hold an absolute and a relative path

--- src/main/groovy/io/gitlab/arturbosch/jpal/ast/source/SourcePath.groovy
+++ src/main/groovy/io/gitlab/arturbosch/jpal/ast/source/SourcePath.groovy
@@ -3,6 +3,7 @@ package io.gitlab.arturbosch.jpal.ast.source
 import groovy.transform.CompileStatic
 import groovy.transform.Immutable
 import groovy.transform.ToString
+import io.gitlab.arturbosch.jpal.core.CompilationInfo
 
 import java.nio.file.Path
 
@@ -17,9 +18,14 @@ import java.nio.file.Path
 class SourcePath {
 
 	String path
+	String relativePath
 
-	static SourcePath of(Path path) {
-		return new SourcePath(path.toString())
+	static SourcePath of(Path path, Path relativePath) {
+		return new SourcePath(path.toString(), relativePath.toString())
+	}
+
+	static SourcePath of(CompilationInfo info) {
+		return new SourcePath(info.path.toString(), info.relativePath.toString())
 	}
 
 	@Override
